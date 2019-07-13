df4341edf0b1616c5ab8fca4c878746027431eab
Tony Werner <dm000nijebovckiy@gmail.com>
2019-07-13 19:29:10 +0300

    Added PVS Studio comment

--- Tester/TestRunner.cs
+++ Tester/TestRunner.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿// This is an independent project of an individual developer. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++, C#, and Java: http://www.viva64.com
+
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
@@ -93,6 +96,7 @@ namespace Tester
                 error = process.StandardError.ReadToEnd();
 
                 process.WaitForExit();
+                process.Dispose();
             }
             catch (Exception)
             {
@@ -111,9 +115,9 @@ namespace Tester
             {
                 foreach (var pathToTest in allDiscoveredTests)
                 {
-                    var testName = Path.GetFileNameWithoutExtension(pathToTest);
+                    var fileName = Path.GetFileNameWithoutExtension(pathToTest);
 
-                    if (test.Name == testName)
+                    if (test.Name == fileName)
                     {
                         pathsToTests.Add(test.Name, pathToTest);
                     }
